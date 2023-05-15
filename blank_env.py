659ff45c1d97ff4c8917ac3664a75a8115c7e7c8
Aaron Moore <aaron.mv.moore@gmail.com>
2023-05-15 15:10:17 -0500

    Add work

--- blank_env.py
+++
@@ -1,8 +0,0 @@
-host = 'string host name'
-user = 'string username'
-password = 'string password'
-
-def get_db_url(db):
-    database_name = db
-    url = f'mysql+pymysql://{user}:{password}@{host}/{database_name}'
-    return url
